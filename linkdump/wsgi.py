09dbdd4bf2f40fb3dd4a8fdd1b8161ed1f82cbb5
Mat Gadd <source@catchall.drarok.com>
2019-01-04 15:39:03 +0000

    Prefer use of .env over existing environment

--- linkdump/wsgi.py
+++ linkdump/wsgi.py
@@ -12,7 +12,10 @@ import os
 import dotenv
 from django.core.wsgi import get_wsgi_application
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+dotenv.read_dotenv(
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'),
+    override=True,
+)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linkdump.settings")
 
