ad8c34d492575db68e20be4e70ab324f13dd8992
Mat Gadd <source@catchall.drarok.com>
2019-01-04 14:07:45 +0000

    Use .env file over actual environment vars

--- linkdump/settings.py
+++ linkdump/settings.py
@@ -13,7 +13,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 import dj_database_url
+import dotenv
 
+
+# Use .env file with precedence over actual environment
+dotenv.read_dotenv(override=True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
